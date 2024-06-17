8ee132a527e801f5b42603f4505f44471b2da00f
Raphaël Tétreault <41877613+RaphaelTetreault@users.noreply.github.com>
2024-06-17 00:11:59 -0400

    Add Visual Studio project along with WIP code

---
+++ GAME 10003 Game Develpoment Foundations - 2D Project Template/Game.cs
@@ -0,0 +1,29 @@
+﻿using Raylib_cs;
+using System;
+using System.Numerics;
+
+public class Game
+{
+    // Variables needed to set up raylib
+    public string title = "Game Title";
+    public int screenWidth = 800;
+    public int screenHeight = 600;
+    public int targetFps = 60;
+    public Color backgroundColor = new Color(240);
+
+    // Place your variables here
+
+
+    // Setup runs once before the game loop begins.
+    public void Setup()
+    {
+
+    }
+
+    // Update runs every frame.
+    public void Update()
+    {
+        Raylib.DrawTextEx(Program.monospaceFont, "testing", new Vector2(5, 5), 16, 0, Color.Black);
+        Text.Draw("Hello", new(200, 200));
+    }
+}
---
+++ GAME 10003 Game Develpoment Foundations - 2D Project Template/Program.cs
@@ -0,0 +1,44 @@
+﻿/*////////////////////////////////////////////////////////////////////////
+ * Raylib-CS Template
+ * Copyright (c)
+ * Mohawk College, 135 Fennell Ave W, Hamilton, Ontario, Canada L9C 0E5
+ * Game Design (374): GAME 10003 Game Development Foundations
+ * 
+ * Written by:
+ *      Raphaël Tétreault
+ * History:
+ *      2024/05/26: Initial version
+ *////////////////////////////////////////////////////////////////////////
+
+using Raylib_cs;
+using System;
+using System.IO;
+
+public class Program
+{
+    public static Font monospaceFont;
+
+    static void Main()
+    {
+        // Create game instance
+        Game game = new();
+
+        Raylib.InitWindow(game.screenWidth, game.screenHeight, game.title);
+        Raylib.InitAudioDevice();
+        Raylib.SetTargetFPS(game.targetFps);
+        Text.Initialize();
+
+        game.Setup();
+        while (!Raylib.WindowShouldClose())
+        {
+            Raylib.SetWindowSize(game.screenWidth, game.screenHeight);
+            Raylib.BeginDrawing();
+            Raylib.ClearBackground(game.backgroundColor);
+            game.Update();
+            Raylib.EndDrawing();
+        }
+
+        Raylib.CloseAudioDevice();
+        Raylib.CloseWindow();
+    }
+}
