2ac9f78de5337e88ee8aafa409611acbb9760344
Raphaël Tétreault <41877613+RaphaelTetreault@users.noreply.github.com>
2024-09-02 22:48:10 -0400

    Add XML comments to Graphics functions

--- GAME 10003 Game Develpoment Foundations - 2D Project Template/Graphics.cs
+++ GAME 10003 Game Develpoment Foundations - 2D Project Template/Graphics.cs
@@ -9,8 +9,17 @@ using System.Numerics;
 /// </remarks>
 public static class Graphics
 {
+    /// <summary>
+    ///     Angle rotation of graphics in degrees.
+    /// </summary>
     public static float Rotation { get; set; } = 0;
+    /// <summary>
+    ///     Scale of graphics. Default is 1.
+    /// </summary>
     public static float Scale { get; set; } = 1;
+    /// <summary>
+    ///     Color tint of graphics. DEfault is white.
+    /// </summary>
     public static Color Tint { get; set; } = Color.White;
 
     /// <summary>
@@ -39,14 +48,39 @@ public static class Graphics
         Raylib.UnloadTexture(texture);
     }
 
+    /// <summary>
+    ///     Draw a <paramref name="texture"/> graphic to the screen at
+    ///     <paramref name="position"/>.
+    /// </summary>
+    /// <param name="texture">The texture to draw.</param>
+    /// <param name="position">The position to draw at.</param>
     public static void Draw(Texture2D texture, Vector2 position)
     {
         Raylib.DrawTextureEx(texture, position, Rotation, Scale, Tint);
     }
 
+    /// <summary>
+    ///     Draw a <paramref name="texture"/> graphic to the screen at
+    ///     position (<paramref name="x"/>, <paramref name="y"/>).
+    /// </summary>
+    /// <param name="texture">The texture to draw.</param>
+    /// <param name="x">The X position to draw at.</param>
+    /// <param name="y">The Y position to draw at.</param>
     public static void Draw(Texture2D texture, float x, float y)
         => Draw(texture, new Vector2(x, y));
 
+    /// <summary>
+    ///     Draw a subset of <paramref name="texture"/> graphic at screen
+    ///     <paramref name="position"/>. Subset begins at upper-left corner
+    ///     <paramref name="subsetOrigin"/> and size of <paramref name="subsetSize"/>.
+    ///     <paramref name="rotationOrigin"/> is the point around which the
+    ///     subset is rotated; default is upper-left corner.
+    /// </summary>
+    /// <param name="texture">The texture to draw.</param>
+    /// <param name="position">The position to draw at.</param>
+    /// <param name="subsetOrigin">Subset origin within the texture.</param>
+    /// <param name="subsetSize">Subset size within the texture.</param>
+    /// <param name="rotationOrigin">Rotation origin of the texture subset.</param>
     public static void DrawSubset(Texture2D texture, Vector2 position, Vector2 subsetOrigin, Vector2 subsetSize, Vector2 rotationOrigin)
     {
         // Source in texture/spritesheet/atlas
@@ -64,7 +98,15 @@ public static class Graphics
         Raylib.DrawTexturePro(texture, source, destination, -rotationOrigin, Rotation, Tint);
     }
 
-    // w/o rotation origin
+    /// <summary>
+    ///     Draw a subset of <paramref name="texture"/> graphic at screen
+    ///     <paramref name="position"/>. Subset begins at upper-left corner
+    ///     <paramref name="subsetOrigin"/> and size of <paramref name="subsetSize"/>.
+    /// </summary>
+    /// <param name="texture">The texture to draw.</param>
+    /// <param name="position">The position to draw at.</param>
+    /// <param name="subsetOrigin">Subset origin within the texture.</param>
+    /// <param name="subsetSize">Subset size within the texture.</param>
     public static void DrawSubset(Texture2D texture, Vector2 position, Vector2 subsetOrigin, Vector2 subsetSize)
         => DrawSubset(texture, position, subsetOrigin, subsetSize, Vector2.Zero);
 }
