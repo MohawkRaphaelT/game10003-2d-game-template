e16629bfe54079759967e1c58f5cf605037b1c38
Raphaël Tétreault <41877613+RaphaelTetreault@users.noreply.github.com>
2024-09-06 22:10:07 -0400

    Clean up Game template

--- GAME 10003 Game Development Foundations - 2D Project Template/Game.cs
+++ GAME 10003 Game Development Foundations - 2D Project Template/Game.cs
@@ -1,29 +1,29 @@
-﻿using System;
+﻿// Include code libraries you need here:
+using System;
 using System.Numerics;
 
-// Your game code goes inside this class!
+
+/// <summary>
+///     Your game code goes inside this class!
+/// </summary>
 public class Game
 {
-    // Place your variables here
-    
+    // Place your variables here:
 
-    // Setup runs once before the game loop begins.
+
+    /// <summary>
+    ///     Setup runs once before the game loop begins.
+    /// </summary>
     public void Setup()
     {
-        Window.TargetFPS = 30;
-        Window.SetSize(400, 400);
+
     }
 
-    // Update runs every frame.
+    /// <summary>
+    ///     Update runs every frame.
+    /// </summary>
     public void Update()
     {
-        Window.ClearBackground(Color.White);
 
-        Draw.LineColor = Color.Black;
-        Draw.FillColor = Color.Red;
-        Draw.Triangle(new(100, 100), 50, Time.SecondsElapsed);
-        Draw.Square(20, 20, 50);
-
-        Raylib_cs.Raylib.DrawCircle(200, 200, 50, Color.Blue);
     }
 }
