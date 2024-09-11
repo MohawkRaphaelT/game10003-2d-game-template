936971941c1a8269c1b5b6f2f47d426aefb5699a
Raphaël Tétreault <41877613+RaphaelTetreault@users.noreply.github.com>
2024-09-11 19:35:18 -0400

    Remove temp code in Game

--- GAME 10003 Game Development Foundations - 2D Project Template/Game.cs
+++ GAME 10003 Game Development Foundations - 2D Project Template/Game.cs
@@ -18,7 +18,7 @@ public class Game
     /// </summary>
     public void Setup()
     {
-        Window.SetSize(400, 400);
+
     }
 
     /// <summary>
@@ -26,9 +26,6 @@ public class Game
     /// </summary>
     public void Update()
     {
-        Window.ClearBackground(Color.OffWhite);
-        Draw.FillColor = Color.Red;
-        Draw.Triangle(10, 10, 110, 10, 55, 100);
-        Draw.Quad(10, 200, 110, 200, 110, 300, 10, 300);
+
     }
 }
