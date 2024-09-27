5027247f9e7921278e64f45924e345d119ecd2cd
Raphaël Tétreault <41877613+RaphaelTetreault@users.noreply.github.com>
2024-09-26 22:00:02 -0400

    m: Update comments in Controller* enums

--- GAME 10003 Game Development Foundations - 2D Project Template/Game10003/Enums/ControllerAxis.cs
+++ GAME 10003 Game Development Foundations - 2D Project Template/Game10003/Enums/ControllerAxis.cs
@@ -15,32 +15,32 @@ namespace Game10003;
 public enum ControllerAxis
 {
     /// <summary>
-    ///     Controller left stick horizontal axis
+    ///     Controller left stick horizontal axis.
     /// </summary>
     LeftX,
 
     /// <summary>
-    ///     Controller left stick vertical axis
+    ///     Controller left stick vertical axis.
     /// </summary>
     LeftY,
 
     /// <summary>
-    ///     Controller right stick horizontal axis
+    ///     Controller right stick horizontal axis.
     /// </summary>
     RightX,
 
     /// <summary>
-    ///     Controller right stick vertical axis
+    ///     Controller right stick vertical axis.
     /// </summary>
     RightY,
 
     /// <summary>
-    ///     Controller left side back trigger, pressure level: 1 through -1
+    ///     Controller left side back trigger, pressure level: 1 through -1.
     /// </summary>
     LeftTrigger,
 
     /// <summary>
-    ///     Controller right side back trigger, pressure level: 1 through -1
+    ///     Controller right side back trigger, pressure level: 1 through -1.
     /// </summary>
     RightTrigger
 }
