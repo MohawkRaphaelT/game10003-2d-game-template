ede19ab7443138d01842739c6ba985c66cac15f0
Raphaël Tétreault <41877613+RaphaelTetreault@users.noreply.github.com>
2024-10-09 18:47:37 -0400

    WIP create singlebuffer window

--- GAME 10003 Game Development Foundations - 2D Project Template/Game10003/Classes/Program.cs
+++ GAME 10003 Game Development Foundations - 2D Project Template/Game10003/Classes/Program.cs
@@ -6,6 +6,7 @@
 
 using Game10003;
 using Raylib_cs;
+using System.Numerics;
 
 /// <summary>
 ///     The main underlying program. DO NOT EDIT.
@@ -13,6 +14,8 @@ using Raylib_cs;
 [GeneratorTools.OmitFromDocumentation]
 public static class Program
 {
+    const int MaxRenderSize = 800;
+
     private static void Main()
     {
         // Create game instance
@@ -25,7 +28,26 @@ public static class Program
 
         // Wrapper setup
         Text.Initialize();
+
+        // Get 2 render textures used to draw to screen
+        bool drawBuffer0 = true;
+        RenderTexture2D[] buffers =
+        {
+            Raylib.LoadRenderTexture(MaxRenderSize, MaxRenderSize),
+            Raylib.LoadRenderTexture(MaxRenderSize, MaxRenderSize),
+        };
+        // Init: set window size and inital background draw
+        Raylib.BeginTextureMode(buffers[0]);
         game.Setup();
+        Draw.FillColor = Game10003.Color.Blue;
+        Draw.Square(0, 0, 100);
+        Raylib.EndTextureMode();
+        // Copy contents to other buffer
+        Raylib.BeginTextureMode(buffers[1]);
+        Raylib.DrawTexture(buffers[0].Texture, 0, 0, Raylib_cs.Color.White);
+        Draw.FillColor = Game10003.Color.Green;
+        Draw.Square(0, 0, 100);
+        Raylib.EndTextureMode();
 
         // Raylib & wrapper frame loop
         while (!Raylib.WindowShouldClose())
@@ -34,10 +56,21 @@ public static class Program
             foreach (var music in Audio.LoadedMusic)
                 Raylib.UpdateMusicStream(music);
 
-            // Run game frame
-            Raylib.BeginDrawing();
+            // Choose current buffer
+            RenderTexture2D thisFrame = drawBuffer0 ? buffers[0] : buffers[1];
+            RenderTexture2D lastFrame = drawBuffer0 ? buffers[1] : buffers[0];
+            Rectangle renderArea = new Rectangle(MaxRenderSize - Window.Width, MaxRenderSize - Window.Height, Window.Width, -Window.Height);
+            // Start frame with contens of previous frame
+            Raylib.BeginTextureMode(thisFrame);
+            Raylib.DrawTextureRec(lastFrame.Texture, renderArea, Vector2.Zero, Raylib_cs.Color.White);
             game.Update();
+            Raylib.EndTextureMode();
+            // Render to screen
+            Raylib.BeginDrawing();
+            Raylib.DrawTextureRec(thisFrame.Texture, renderArea, Vector2.Zero, Raylib_cs.Color.White);
             Raylib.EndDrawing();
+            // Swap buffers
+            drawBuffer0 = !drawBuffer0;
 
             // Update rendered frame count
             Time.FramesElapsed++;
