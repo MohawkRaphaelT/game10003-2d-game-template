ec813efa92c4d456ea7be5abac3a936cb4e97c7b
Raphaël Tétreault <41877613+RaphaelTetreault@users.noreply.github.com>
2026-09-01 16:22:57 -0400

    Add Draw.SetFillColor and related functions and overrides for variable-less assignment.
    - Update copyright for all files

--- GAME 10003 Game Development Foundations - 2D Project Template/MohawkGame2D/Enums/MouseButton.cs
+++ GAME 10003 Game Development Foundations - 2D Project Template/MohawkGame2D/Enums/MouseButton.cs
@@ -1,8 +1,9 @@
 ﻿/*////////////////////////////////////////////////////////////////////////
- * Copyright (c)
- * Mohawk College, 135 Fennell Ave W, Hamilton, Ontario, Canada L9C 0E5
- * Game Design (374): GAME 10003 Game Development Foundations
- *////////////////////////////////////////////////////////////////////////
+/* Copyright (c)
+/* Mohawk College, 135 Fennell Ave W, Hamilton, Ontario, Canada L9C 0E5
+/* Game Design (374): GAME 10003 Game Development Foundations
+/* Source: https://github.com/MohawkRaphaelT/game10003-2d-game-template
+/*////////////////////////////////////////////////////////////////////////
 
 namespace MohawkGame2D;
 
@@ -12,7 +13,7 @@ namespace MohawkGame2D;
 /// <remarks>
 ///     Wrapper around Raylib's MouseButton
 /// </remarks>
-public enum MouseInput
+public enum MouseButton
 {
     /// <summary>
     ///     Left mouse button.
